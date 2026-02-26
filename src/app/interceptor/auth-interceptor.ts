1fcd20827d87272eaa4af305cd1f67f3b8154f7c
hdqtonz <hiten.qtonz@yahoo.com>
2026-02-26 15:32:54 +0530

    ngrx - 1 file changed - fixed logout error in auth effect

--- src/app/interceptor/auth-interceptor.ts
+++ src/app/interceptor/auth-interceptor.ts
@@ -3,11 +3,12 @@ import { inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../_store/app.state';
 import { getToken } from '../auth/state/auth.selector';
-import { exhaust, exhaustMap, switchMap } from 'rxjs';
+import { exhaustMap, take } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const store = inject(Store<AppState>);
   return store.select(getToken).pipe(
+    take(1),
     exhaustMap((token) => {
       if (!token) {
         return next(req);
